de4bcde562d8d079ce98daa7a0e3b78179a6d829
alanglinais7 <austinlanglinais@Austins-MacBook-Pro-2.local>
2021-12-13 21:55:21 -0800

    Small little updates

--- routes.js
+++ routes.js
@@ -3,7 +3,9 @@ const routes =  require('next-routes')();
 // the colon is the wildcard
 routes
   .add('/campaigns/new', 'campaigns/new')
-  .add('/campaigns/:address', '/campaigns/show');
+  .add('/campaigns/:address', '/campaigns/show')
+  .add('/campaigns/:address/requests', '/campaigns/requests/index')
+  .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 
 
 module.exports = routes;
